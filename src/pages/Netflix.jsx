4dc5718d3a19e919ebab2fd9212b710dd928291d
Facundo <facundogatti87@gmail.com>
2022-11-28 22:25:25 -0300

    version estable

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -6,15 +6,25 @@ import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import {useDispatch } from "react-redux";
+import { getGenres } from "../store";
 
 export default function Netflix() {
   const [isScrolled, setIsScrolled] = useState(false);
   const navigate = useNavigate();
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGenres());
+  }, [dispatch]);
+
+  // FUNCION PARA QUE LA NAVBAR SE OCULTE AL BAJAR EL MOUSE
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
     return () => (window.onscroll = null);
   };
+  /////////////////////////////////////////////////////////
 
   return (
     <Container>
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-import { async } from "@firebase/util";
 import {
   configureStore,
   createAsyncThunk,
@@ -6,13 +5,18 @@ import {
 } from "@reduxjs/toolkit";
 import { API_KEY, TMBD_BASE_URL } from "../utils/constants";
 import axios from "axios";
-
 ////////////// OBTENGO CATEGORIAS DE PELICULAS
 const initialState = {
   movies: [],
   genresLoaded: false,
   genres: [],
 };
+
+const NetflixSlice = createSlice({
+  name: "Netflix",
+  initialState,
+  extraReducers: (builder) => {},
+});
 
 export const getGenres = createAsyncThunk("netflix/genres", async () => {
   const {
@@ -21,64 +25,8 @@ export const getGenres = createAsyncThunk("netflix/genres", async () => {
   return genres;
 });
 
-const createArrayFromRawData = (array, moviesArray, genres) => {
-  console.log(array);
-  array.forEach((movie) => {
-    const movieGenres = [];
-    movie.genre_ids.forEach((genre) => {
-      const name = genres.find(({ id }) => id === genre);
-      if (name) movieGenres.push(name.name);
-    });
-    if (movie.backdrop_path) {
-      moviesArray.push({
-        id: movie.id,
-        name: movie?.original_name ? movie.original_name : movie.original_title,
-        image: movie.backdrop_path,
-        genres: movieGenres.slice(0, 3),
-      });
-    }
-  });
-};
-
-const getRawData = async (api, genres, paging) => {
-  const moviesArray = [];
-  for (let i = 1; moviesArray.length < 60 && i < 10; i++) {
-    const {
-      data: { results },
-    } = await axios.get(`${api}${paging ? `&page=${i}` : ""}`);
-    createArrayFromRawData(results, moviesArray, genres);
-    return moviesArray;
-  }
-};
-
-export const fetchMovies = createAsyncThunk(
-  "netflix/trending",
-  async ({ type }, thunkApi) => {
-    const {
-      netflix: { genres },
-    } = thunkApi.getState();
-    const data = getRawData(
-      `${TMBD_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
-      genres,
-      true
-    );
-  }
-);
-
-const NetflixSlice = {
-  name: "Netflix",
-  initialState,
-  extraReducers: (builder) => {
-    builder.addCase(getGenres.fulfilled, (state, action) => {
-      state.genres = action.payload;
-      state.genresLoaded = true;
-    });
-  },
-};
-
 export const store = configureStore({
   reducer: {
     netflix: NetflixSlice.reducer,
   },
 });
-//////////////////////////////////////////////
